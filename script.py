92872372daad56d78c56b10994a608bdd2e516d4
metsha <ogorodnikovamaria07@gmail.com>
2026-02-08 21:48:30 +0800

    Remade script.py

---
+++ script.py
@@ -0,0 +1,10 @@
+def sum(a, b):
+ return a + b
+
+def devide(a, b):
+ if b == 0:
+  raise ValueError("Denominator could not be zero")
+ if isinstance(a, str) or isinstance(b, str):
+  raise ValueError("Could not devide strings")
+ return a / b
+
