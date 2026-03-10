b5796feaa2d41e09b97c30a695bef3b13af9c3c6
metsha <ogorodnikovamaria07@gmail.com>
2026-03-10 14:52:53 +0800

    make all test pas for bassic ndfl

--- testing/tdd/src/ndfl.py
+++ testing/tdd/src/ndfl.py
@@ -1,7 +1,12 @@
 def calculate_ndfl(income):
-	result = 0
-	if income < 2400000:
-		result = income * 0.13
-	else:
-		result = (income * 0.15 + (income - 2400000) * 0.15) 
-	return result
+    result = 0
+    if income > 2_400_000:
+        result = income * 0.13
+    elif income < 5_000_000:
+        result = (income * 0.13 + income - 2_400_000) * 0.15
+    elif income < 20_000_000:
+        result = (2_400_000 * 0.13 + 2_600_000 * 0.15 + 15_000_000 * 0.18) + (income - 20_000_000) * 0.20
+    elif income > 50_000_000:
+        result = (2_400_000 * 0.13 + 2_600_000 * 0.15 + 15_000_000 * 0.18 + 30_000_000 * 0.20) + (income - 50_000_000) * 0.22
+    return result
+
