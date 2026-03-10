f03a5fcbf42b69f30afd9e2588777be06ca1645d
metsha <ogorodnikovamaria07@gmail.com>
2026-03-10 14:35:49 +0800

    added ndfl and test_bank

---
+++ testing/tdd/tests/test_bank_ndfl.py
@@ -0,0 +1,13 @@
+from ndfl import calculate_ndfl
+
+def test_ndfl_tier_1_basic():
+	assert calculate_ndfl(2000000) == 200000
+
+def test_ndfl_tier_2_basic():
+	assert calculate_ndfl(4000000) == 552000
+def test_ndfl_tier_3_basic():
+        assert calculate_ndfl(10000000) == 1602000
+def test_ndfl_tier_4_basic():
+        assert calculate_ndfl(30000000) == 5402000
+def test_ndfl_tier_5_basic():
+        assert calculate_ndfl(60000000) == 11602000
