1de5e9b32b815a53b2cd47d9c072519078af5404
metsha <ogorodnikovamaria07@gmail.com>
2026-04-02 13:16:58 +0800

    added docsting example

--- docs_domain/src/sales.py
+++ docs_domain/src/sales.py
@@ -1,7 +1,20 @@
 # TODO refactor this module using buisness logic names
 
 
-def _parse_record(line: str):
+def _parse_record(line: str) -> dict | None:
+    """ Parsing information for only sale
+
+    Parametrs:
+        line - string that contain sale record in form of comma separated values
+
+    Returns:
+        sale - sale information in form of dict
+
+    Raises:
+        ValueError
+    """
+    if line == "":
+        raise ValueError("Got zero length line")
     sale = line.strip().split(",")  
     if len(sale) != 4:  # according specs each sale is defined by four fields
         return None  
@@ -11,6 +24,8 @@ def _parse_record(line: str):
     try: 
         unit_price = float(sale[2])  
         quantity = int(sale[3])
+        if int(sale[3]) != float(sale[3]):
+            return None
     except ValueError:
         return None 
 
